285b40808af3452170da148467ad147fb3a3de2a
breeze37 <arnabsen731@gmail.com>
2023-08-28 22:32:57 +0530

    27/08

---
+++ A_Divisibility_Problem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int moves=0;
+        if(a%b!=0){
+            a = a+1;
+            
+        cout<<moves<<endl;
+    }
+}
---
+++ A_Elephant.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    int path = (x+4)/5;
+    cout << path;
+}
